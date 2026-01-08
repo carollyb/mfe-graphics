2224be8a3e3683f4e31107304e0d7280248701fa
carolly <carolly.barbosa@gmail.com>
2026-01-08 19:55:37 -0300

    chore: adds config to avoid cors errors

--- vite.config.ts
+++ vite.config.ts
@@ -11,4 +11,12 @@ export default defineConfig({
       },
     },
   },
+  server: {
+    cors: true,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+      'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+    },
+  },
 });
